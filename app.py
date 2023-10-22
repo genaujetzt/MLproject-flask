d00a1ec125c8888754e3e7a0621b97c779c5b6c2
genaujetzt <tracytheplane@gmail.com>
2023-10-22 22:46:16 +0300

    third version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import sklearn
+import xgboost
 import pickle
 app = Flask(__name__)
 loaded_model = pickle.load(open('my_model.pickle', "rb"))
