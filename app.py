ba8d8b0b159eb5b6365a76c84ab772c017c0d232
genaujetzt <tracytheplane@gmail.com>
2023-10-22 03:17:22 +0300

    second version

--- app.py
+++ app.py
@@ -19,4 +19,3 @@ def predict():
         print(pred)
         message = f"Width will be around {pred[0][0]} and Depth will be around {pred[0][1]}"
     return render_template('index.html', message = message)
-
