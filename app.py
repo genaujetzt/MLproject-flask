fbfb3e6df3833d3c364e5b7f70670c85c9821d20
genaujetzt <tracytheplane@gmail.com>
2023-10-23 05:40:59 +0300

    final version

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
-import sklearn
-import xgboost
+from sklearn import tree
+from sklearn.preprocessing import MinMaxScaler
 import pickle
 app = Flask(__name__)
 loaded_model = pickle.load(open('TreeRegressor.pickle', "rb"))
@@ -14,9 +14,12 @@ def predict():
         IF = request.form.get('IF')
         VW = request.form.get('VW')
         FP = request.form.get('FP')
-        data = [[float(IW), float(IF), float(VW), float(FP)]]
-        data = loaded_scaler.transform(data)
-        pred = loaded_model.predict(data)
-        print(pred)
-        message = f"Width will be around {pred[0][0]} and Depth will be around {pred[0][1]}"
+        if IW == "" or IF =='' or VW =='' or FP=='':
+            message = 'You didn`t enter proper values'
+        else:
+            data = [[float(IW), float(IF), float(VW), float(FP)]]
+            data = loaded_scaler.transform(data)
+            pred = loaded_model.predict(data)
+            message = f"Width will be around {pred[0][0]} and Depth will be around {pred[0][1]}"
     return render_template('index.html', message = message)
+app.run()
