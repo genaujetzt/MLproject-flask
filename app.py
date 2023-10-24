ef3c1aeabbba9fa6ea3ea6b4f7947fd711c21fe9
genaujetzt <tracytheplane@gmail.com>
2023-10-24 19:03:20 +0300

    fixed errors

--- app.py
+++ app.py
@@ -7,19 +7,18 @@ app = Flask(__name__)
 loaded_model = pickle.load(open('TreeRegressor.pickle', "rb"))
 loaded_scaler = pickle.load(open("my_scaler.pickle", "rb"))
 @app.route('/', methods = ["GET", "POST"])
-def predict():
+def return_prediction():
     message = ""
     if request.method == "POST":
         IW = request.form.get('IW')
         IF = request.form.get('IF')
         VW = request.form.get('VW')
         FP = request.form.get('FP')
-        if IW == "" or IF =='' or VW =='' or FP=='':
-            message = 'You didn`t enter proper values'
+        if not (IW and IF and VW and FP):
+            message = "You didn't enter proper values"
         else:
             data = [[float(IW), float(IF), float(VW), float(FP)]]
             data = loaded_scaler.transform(data)
             pred = loaded_model.predict(data)
             message = f"Width will be around {pred[0][0]} and Depth will be around {pred[0][1]}"
     return render_template('index.html', message = message)
-app.run()
